bf4a2c105a19cefa327e98c852e9a26042d6b9ce
zjb4800 <3607527046@qq.com>
2022-02-07 18:06:14 +0800

    Changes-first

---
+++ src/main/java/com/zjb/git_idea_test/GitIdeaTestApplication.java
@@ -0,0 +1,13 @@
+package com.zjb.git_idea_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIdeaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIdeaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjb/git_idea_test/GitIdeaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjb.git_idea_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
